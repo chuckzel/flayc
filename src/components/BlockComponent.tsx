a3b66afa52674134c7d5af2fc047ae197b645aa6
chuckzel <126327655+chuckzel@users.noreply.github.com>
2026-06-21 13:56:35 +0200

    Set page aspect ratio according to size

--- src/components/BlockComponent.tsx
+++ src/components/BlockComponent.tsx
@@ -32,7 +32,15 @@ function PageBlockComponent({ block }: { block: PageBlock }) {
   ) {
     childrenHtml.push(<BlockComponent key={child.id} block={child} />);
   }
-  return <div>page ({childrenHtml})</div>;
+  const style = {
+    color: "black",
+    aspectRatio: `${block.fields.WIDTH} / ${block.fields.HEIGHT}`,
+  } as React.CSSProperties;
+  return (
+    <div className="bg-white" style={style}>
+      page ({childrenHtml})
+    </div>
+  );
 }
 
 function ContainerBlockComponent({ block }: { block: ContainerBlock }) {
